f8c705fdfda4877fa340b5213a09c66990f948ca
Nevo Segal <nevosegal@gmail.com>
2016-05-20 23:59:19 +0100

    moved things around

---
+++ dft.js
@@ -0,0 +1,21 @@
+'use strict';
+
+let utils = require('./utils');
+let X = [];
+
+function dft(signal){
+  let N = signal.length;
+
+  for (var k = 0; k < N; k++) {
+    X[k] = [0.0, 0.0];
+
+    signal.map((sample, n) => {
+      let temp = utils.multiply([sample, 0.0], utils.euler(k*n, N));
+      X[k] = utils.add(X[k], temp);
+    });
+  }
+
+  return X;
+}
+
+module.exports = dft;
--- fft.js
+++ fft.js
@@ -1,19 +1,3 @@
 'use strict';
-
-let utils = require('./utils');
-let X = [];
-
-function dft(signal){
-  let N = signal.length;
-
-  for (var k = 0; k < N; k++) {
-    X[k] = [0.0, 0.0];
-
-    signal.map((sample, n) => {
-      let temp = utils.multiply([sample, 0.0], utils.euler(k*n, N));
-      X[k] = utils.add(X[k], temp);
-    });
-  }
-
-  return X;
-}
+let dft = require('./dft');
+console.log(dft([0,1,2,3,4,5,6,7]));
