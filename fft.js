04b655a723be02bb03b0cfe24a84b8acf482e609
Nevo Segal <nevosegal@gmail.com>
2016-05-31 11:28:19 +0100

    added bit reversal premutation of a fixed binary length

--- fft.js
+++ fft.js
@@ -1,3 +1,5 @@
 'use strict';
 let dft = require('./dft');
-console.log(dft([0,1,2,3,4,5,6,7]));
+let utils = require('./utils');
+
+console.log(utils.bitReverseArray(16));
--- utils.js
+++ utils.js
@@ -1,22 +1,42 @@
 'use strict';
 
+var memoizedReversal = {};
+let bitReverseArray = function(N){
+	if(memoizedReversal[N] === undefined){
+		let maxBinaryLength = (N-1).toString(2).length;
+		let templateBinary = '0'.repeat(maxBinaryLength);
+		let reversed = {};
+		for(let n=0; n<N; n++){
+			let currBinary = n.toString(2);
+			currBinary = templateBinary.substr(currBinary.length) + currBinary;
+			currBinary = [...currBinary].reverse().join('');
+			reversed[n] = parseInt(currBinary, 2);
+		}
+		memoizedReversal[N] = reversed;
+	}
+	return memoizedReversal[N];
+}
+
 // complex multiplication
 let multiply = function(a,b){
 	return [a[0]*b[0] - a[1]*b[1], 
 			a[0]*b[1] + a[1]*b[0]];
 }
 
+// complex addition
 let add = function(a,b){
   return [a[0]+b[0], a[1]+b[1]];
 }
 
+// euler's identity e^x = cos(x) + sin(x)
 let euler = function(kn, N){
 	let x = -2*Math.PI*kn/N;
-  return [Math.cos(x), Math.sin(x)];
+	return [Math.cos(x), Math.sin(x)];
 }
 
 module.exports={
+	bitReverseArray: bitReverseArray,
 	multiply: multiply,
-  add: add,
-  euler: euler
+  	add: add,
+  	euler: euler
 };
