c92a6b2c48720db33b1c8190fd73afcac65cfefa
Nevo Segal <nevosegal@gmail.com>
2016-06-02 09:27:29 +0100

    sorting array in advance, making real signal complex

--- fft.js
+++ fft.js
@@ -7,17 +7,28 @@ let fft = function(signal){
 	let bitReversedIndices = utils.bitReverseArray(N);
 	let logN = Math.log2(N);
 
+	// sort array and store as complex
+	let ordered = [];
+	for(let i=0; i<N; i++) ordered[bitReversedIndices[i]] = signal[i];
+	for(let i=0; i<N; i++) signal[i] = [ordered[i], 0];
+
+
+	// iterate over the number of stages
 	for(let n=1; n<=logN; n++){
 		let currN = Math.pow(2,n);
+
+		// find twiddle factors
 		for(let k=0; k<currN/2; k++){
 			let twiddle = utils.euler(k, currN);
-			for(let m=0; m<N/currN; m++){
-				let currEvenIndex = bitReversedIndices[(currN*m) + k];
-				let currOddIndex = bitReversedIndices[(currN*m) + k + (currN/2)];
-				let odd = utils.multiply(twiddle, [signal[currOddIndex], 0]);
 
-				signal[currOddIndex] = utils.subtract([signal[currEvenIndex],0], odd);
-				signal[currEvenIndex] = utils.add(odd, [signal[currEvenIndex],0]);
+			// on each block of FT, implement the butterfly diagram
+			for(let m=0; m<N/currN; m++){
+				let currEvenIndex = (currN*m) + k;
+				let currOddIndex = (currN*m) + k + (currN/2);
+				let odd = utils.multiply(twiddle, signal[currOddIndex]);
+
+				signal[currOddIndex] = utils.subtract(signal[currEvenIndex], odd);
+				signal[currEvenIndex] = utils.add(odd, signal[currEvenIndex]);
 			}
 		}
 	}
